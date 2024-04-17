9e5bbdca406684f95d8c8c41664d04046cc038e0
Goodness Duru <goodyduru@gmail.com>
2024-04-17 13:03:36 +0000

    Allow persistent tun

--- playtun/get.py
+++ playtun/get.py
@@ -2,7 +2,7 @@ import struct
 from fcntl import ioctl
 
 
-def create_tun(name: str):
+def get_tun(name: str, is_create: bool):
     tun = open("/dev/net/tun", "r+b", buffering=0)
     LINUX_IFF_TUN = 0x0001
     LINUX_IFF_NO_PI = 0x1000
@@ -10,7 +10,10 @@ def create_tun(name: str):
     flags = LINUX_IFF_TUN | LINUX_IFF_NO_PI
     ifs = struct.pack("16sH22s", name, flags, b"")
     ioctl(tun, LINUX_TUNSETIFF, ifs)
+    if is_create:
+        LINUX_TUNSETPERSIST = 0x400454CB
+        ioctl(tun, LINUX_TUNSETPERSIST, 1)
     return tun
 
 if __name__ == "__main__":
-    create_tun("playtun")
+    get_tun(b"playtun", True)
