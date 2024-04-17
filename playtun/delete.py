f4e3a6bce572ca0c906a81a9cd67dd3c45168e83
Goodness Duru <goodyduru@gmail.com>
2024-04-17 14:12:40 +0100

    Add tun delete

--- playtun/delete.py
+++ playtun/delete.py
@@ -2,7 +2,7 @@ import struct
 from fcntl import ioctl
 
 
-def get_tun(name: str, is_create: bool) -> int:
+def delete_tun(name: str):
     tun = open("/dev/net/tun", "r+b", buffering=0) # Open the clone device.
     LINUX_IFF_TUN = 0x0001 # We want a tun device
     LINUX_IFF_NO_PI = 0x1000
@@ -10,11 +10,9 @@ def get_tun(name: str, is_create: bool) -> int:
     flags = LINUX_IFF_TUN | LINUX_IFF_NO_PI
     ifs = struct.pack("16sH22s", name, flags, b"")
     ioctl(tun, LINUX_TUNSETIFF, ifs)
-    if is_create:
-        LINUX_TUNSETPERSIST = 0x400454CB
-        ioctl(tun, LINUX_TUNSETPERSIST, 1)
-    return tun
+    LINUX_TUNSETPERSIST = 0x400454CB
+    ioctl(tun, LINUX_TUNSETPERSIST)
 
 if __name__ == "__main__":
-    tun_fd = get_tun(b"playtun", True)
-    print(f"Tun device playtun has fd of {tun_fd}")
+    tun_fd = delete_tun(b"playtun")
+    print(f"Tun device playtun has been deleted")
