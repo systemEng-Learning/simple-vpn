7fd56fafb38053c9c5687f9e714410f75b716868
steveoni <steohenoni2@gmail.com>
2024-05-22 22:40:08 +0100

    test socket implementation

--- vpn/src/main.rs
+++ vpn/src/main.rs
@@ -1,5 +1,5 @@
 use std::net::Ipv4Addr;
-use vpn::TunSocket;
+use vpn::{SocketFd, TunSocket};
 
 pub fn main() {
     println!("Hello, world!");
@@ -8,9 +8,35 @@ pub fn main() {
     tun.set_netmask(Ipv4Addr::new(255, 255, 255, 0)).unwrap();
     tun.enabled(true).unwrap();
 
+    let socket = SocketFd::new().unwrap();
+
     let mut buf = [0; 4096];
     loop {
+        // packet recieved from tun
         let amount = tun.read(&mut buf).unwrap();
-        println!("{:?}", &buf[0..amount]);
+        let slice = Ipv4HeaderSlice::from_slice(&buf[0..amount]).unwrap();
+        let source_addr = slice.source_addr();
+        println!(
+            "Packet from: {} to: {}, total length {}",
+            slice.source_addr(),
+            slice.destination_addr(),
+            slice.total_len()
+        );
+
+        let server_ip = source_addr.octets();
+        let server_ip = (server_ip[0], server_ip[1], server_ip[2], server_ip[3]);
+
+        if amount > 0 {
+            // replace source_addr with our server address and port
+            socket.send_to(&buf[0..amount], server_ip, 12345);
+        }
+
+        // packet recieved from socket
+        let (recv_amount, recv_buf) = socket.recv_from().unwrap();
+
+        if recv_amount > 0 {
+            let amount = tun.write(&recv_buf);
+            println!("Wrote {} bytes to tun", amount);
+        }
     }
 }
